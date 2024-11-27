c5f8cf0c06790a643564dd8e70e9f9030542943d
dmobley0608 <dmobley0608@gmail.com>
2024-11-27 06:35:58 -0500

    file cleanup

--- app/app/settings.py
+++ app/app/settings.py
@@ -75,8 +75,7 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgres',
-        'PORT': os.environ.get('DB_PORT'),
+        'ENGINE': 'django.db.backends.postgresql',       
         'NAME': os.environ.get("DB_NAME"),
         'USER': os.environ.get("DB_USER"),
         'PASSWORD': os.environ.get("DB_PASS"),
